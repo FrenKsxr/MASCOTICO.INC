214a60a805e32550f2a7598e825a4fb92baa6f29
FrenKsxr <frenyersegura13@outlook.com>
2024-11-19 22:01:17 -0600

    V1.2.1

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,29 @@ import SignUp from './components/SingUp.jsx';
 import Main from './components/MobMenu.jsx'
 import './index.css';
 
+import Home from './page/home.jsx';
+import Footer from './page/home.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
+    <Home />
+
+
+   
+    {/* 
+
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/menu" element={<Main/>}/>
-        <Route path="/signup" element={<SignUp />} /> {/* Ruta para la página de registro */}
+        <Route path="/signup" element={<SignUp />} /> 
       </Routes>
     </BrowserRouter>
+    
+
+    */}
+
   </StrictMode>,
 );
